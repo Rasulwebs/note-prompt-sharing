30765d34238ffde18805d56027ba07f2220f5391
Boboqulov Rasul <boboqulovrasul80@gmail.com>
2024-05-08 02:00:13 +0500

    Update Provider.jsx
    edited style toast

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -6,7 +6,18 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Provider = ({ children, session }) => (
   <SessionProvider session={session}>
-    <ToastContainer />
+    <ToastContainer 
+     position="top-center"
+     autoClose={2000}
+     hideProgressBar={false}
+     newestOnTop={false}
+     closeOnClick
+     rtl={false}
+     pauseOnFocusLoss
+     draggable
+     pauseOnHover
+     theme="colored"
+    />
     {children}
   </SessionProvider>
 );
