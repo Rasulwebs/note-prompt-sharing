a5eb6c8a9f61a62a7fb636553030833552a2a9f9
Alexwebs <boboqulovrasul80@gmail.com>
2024-05-08 01:48:22 +0500

    edited toast

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -15,6 +15,18 @@ const CreatePrompt = () => {
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
+  const notifySuccess = () =>
+    toast.success("Prompt created successfully!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
   const createPrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
